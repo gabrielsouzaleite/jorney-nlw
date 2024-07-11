25ef33b6d48d25843725bdcc9b45916fc946646b
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-07-11 16:24:35 -0300

    Primeiro commit :tada:

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,20 @@
+﻿using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TripsController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RequestRegisterTripJson request)
+    {
+        var useCase = new RegisterTripUseCase();
+        useCase.Execute(request);
+
+        return Created();
+    }
+}
---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,29 @@
+﻿using Journey.Communication.Requests;
+
+namespace Journey.Application.UseCases.Trips.Register;
+
+public class RegisterTripUseCase
+{
+    public void Execute(RequestRegisterTripJson request)
+    {
+        Validate(request);
+    }
+
+    private void Validate(RequestRegisterTripJson request)
+    {
+        if (string.IsNullOrEmpty(request.Name))
+        {
+            throw new ArgumentException("Nome não pode ser vazio!");
+        }
+
+        if (request.StartDate.Date < DateTime.UtcNow.Date)
+        {
+            throw new ArgumentException("A viagem não pode ser registrada para uma data passada!");
+        }
+
+        if (request.EndDate.Date < request.StartDate.Date)
+        {
+            throw new ArgumentException("A viagem de ve terminar após a data de início!");
+        }
+    }
+}
